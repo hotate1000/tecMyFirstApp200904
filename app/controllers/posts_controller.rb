51eadd3e6ed573f8df9a77f99e9fd962b6cefc4a
hotate1000 <ooo.336.sss@gmail.com>
2020-09-05 14:21:01 +0900

    newページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,15 @@ class PostsController < ApplicationController
 
   def new
   end
+
+  def create
+    # Post.create(content: params[:content])
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    # params.permit()
+    params.permit(:content)
+  end
 end
